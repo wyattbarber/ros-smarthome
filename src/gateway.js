d9c3c6b2d0ad874a707b92504109917e766cf6e5
Wyatt Barber <wyattjbarber@gmail.com>
2021-01-24 15:37:14 -0500

    Adding basic folders

---
+++ src/gateway.js
@@ -0,0 +1,7 @@
+const ros = require('rosnodejs');
+const std_msgs = require('std_msgs').msg;
+const smarthome_msgs = require('smarthome').msg;
+
+const express = require('express');
+const app = express();
+
