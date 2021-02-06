e39aa6304a581ac35fba03029c2058e6a210bb17
Wyatt Barber <wyattjbarber@gmail.com>
2021-02-06 16:09:31 -0500

    Not using smarthome app locally anymore

--- src/gateway.js
+++ src/gateway.js
@@ -5,85 +5,77 @@
 // ROS setup
 const ros = require('rosnodejs');
 ros.initNode('smarthome_gateway')
-    .then(nh => {
-        const std_msgs = ros.require('std_msgs');
-        const smarthome_msgs = ros.require('smarthome');
-    });
+  .then(nh => {
+    const std_msgs = ros.require('std_msgs');
+    const smarthome_msgs = ros.require('smarthome');
+  });
 // Web interfaces
 const ngrok = require('ngrok');
 const axios = require('axios');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const {smarthome} = require('actions-on-google');
-const sm_app = smarthome(); 
+
 // Command line interface
 const { exec } = require('child_process');
 
-sm_app.onSync((body, headers) => {
-    ros.log.info('SYNC intent recieved');
-    return {
-        requestId: body.requestId,
-        payload: {
-            agentUserId: "1836.15267389",
-            devices: [{
-              id: "123",
-              type: "action.devices.types.OUTLET",
-              traits: [
-                "action.devices.traits.OnOff"
-              ],
-              name: {
-                defaultNames: ["My Outlet 1234"],
-                name: "Night light",
-                nicknames: ["wall plug"]
-              },
-              willReportState: false,
-              roomHint: "kitchen",
-              deviceInfo: {
-                manufacturer: "lights-out-inc",
-                model: "hs1234",
-                hwVersion: "3.2",
-                swVersion: "11.4"
-              },
-              otherDeviceIds: [{
-                deviceId: "local-device-id"
-              }],
-              customData: {
-                fooValue: 74,
-                barValue: true,
-                bazValue: "foo"
-              }
-            }]
-        }
-    }
+app.post('/', (req, res) => {
+  res.json({
+    user: "1836.15267389",
+    devices: [{
+      id: "123",
+      type: "action.devices.types.OUTLET",
+      traits: [
+        "action.devices.traits.OnOff"
+      ],
+      name: {
+        defaultNames: ["My Outlet 1234"],
+        name: "Night light",
+        nicknames: ["wall plug"]
+      },
+      willReportState: false,
+      roomHint: "kitchen",
+      deviceInfo: {
+        manufacturer: "lights-out-inc",
+        model: "hs1234",
+        hwVersion: "3.2",
+        swVersion: "11.4"
+      },
+      otherDeviceIds: [{
+        deviceId: "local-device-id"
+      }],
+      customData: {
+        fooValue: 74,
+        barValue: true,
+        bazValue: "foo"
+      }
+    }]
+  })
 });
-
-app.use(bodyParser.json(), sm_app).listen(5050);
 
 
 async function send_url() {
-    let public_url;
-    exec("curl http://localhost:4040/api/tunnels", (error, stdout, stderr) => {
-        let data = JSON.parse(stdout);
-        public_url = data.tunnels[0].public_url;
-        console.log(public_url);
+  let public_url;
+  exec("curl http://localhost:4040/api/tunnels", (error, stdout, stderr) => {
+    let data = JSON.parse(stdout);
+    public_url = data.tunnels[0].public_url;
+    console.log(public_url);
 
-        axios({
-            method: 'post',
-            url: "https://us-central1-decent-booster-285122.cloudfunctions.net/connection_refresh",
-            data: {
-                url: public_url,
-                client_id: '12955530',
-                secret: "209j1ncncsc8w0010nijsb0q"
-            }
-        })
-            .then((res) => {
-                //console.log(res);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    });
+    axios({
+      method: 'post',
+      url: "https://us-central1-decent-booster-285122.cloudfunctions.net/connection_refresh",
+      data: {
+        url: public_url,
+        client_id: '12955530',
+        secret: "209j1ncncsc8w0010nijsb0q"
+      }
+    })
+      .then((res) => {
+        //console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
 };
 
 send_url();
